7ed2c776f53b0bc8e2c5355ad8c708ac858176a7
yih237 <yih237@newschool.edu>
2022-02-27 11:25:49 -0500

    -

---
+++ Projects/Project-2/flyaroundbox.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+    animateDiv('.aaa');
+    animateDiv('.bbb');
+
+});
+
+function makeNewPosition(){
+
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 100;
+    var w = $(window).width() - 100;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh,nw];
+
+}
+
+function animateDiv(myclass){
+    var newq = makeNewPosition();
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 800,   function(){
+      animateDiv(myclass);
+    });
+
+};
+
+function calcSpeed(prev, next) {
+
+  var x = Math.abs(prev[1] - next[1]);
+  var y = Math.abs(prev[0] - next[0]);
+
+  var greatest = x > y ? x : y;
+
+  var speedModifier = 0.1;
+
+  var speed = Math.ceil(greatest/speedModifier);
+
+  return speed;
+
+  }
+
+
+
+  

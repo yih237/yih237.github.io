0abcedaf0ef56f81535bd59e00a58cbdeef38b57
yih237 <yih237@newschool.edu>
2022-03-29 10:44:56 -0400

    -

---
+++ assignment/a5/clock1.js
@@ -0,0 +1,33 @@
+function updateBackground() {
+    var 
+      hr = (new Date()).getHours(),
+      body = document.body,
+      bstyle = body.style,    
+      hello = document.querySelector(".hello");    
+    if (hr < 10) {
+      bstyle.backgroundColor = "black";
+      bstyle.color = "white";
+      bstyle.backgroundImage = "url('../morning.png')"
+      hello.innerText = "Morning!";
+    }else if(hr > 10 && hr < 16) {
+        bstyle.backgroundColor = "black";
+        bstyle.color = "white";
+        bstyle.backgroundImage = "url('../day.png')"
+        hello.innerText = "Morning!";
+    }
+     else if (hr > 18 && hr<=20) {
+      bstyle.backgroundColor = "black";
+      bstyle.color = "white";
+      bstyle.backgroundImage = "url('../evening.png')"
+      hello.innerText ="good evening!";
+    } else {
+      bstyle.backgroundColor = "black";
+      bstyle.color = "white";
+      bstyle.backgroundImage = "url('../night.png')"
+
+      hello.innerText = "good night!";
+    } 
+  }
+  
+  setInterval(updateBackground, 1000 * 60);
+  updateBackground();
